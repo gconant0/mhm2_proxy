f6a37326ed9b3169cecb7bfc3e373b4768528571
gavinconant <gavinconant>
2023-10-23 14:40:13 +0000

    Removing buid files

--- .build/upcxx-utils/CMakeFiles/UPCXX_UTILS_GET_VERSION_FILE.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/UPCXX_UTILS_GET_VERSION_FILE"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/UPCXX_UTILS_GET_VERSION_FILE.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
